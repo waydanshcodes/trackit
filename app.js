3917d39cf78a1ba36f0145da26ea64668610e7a8
Vedansh Gupta <waydansh@gmail.com>
2024-07-16 23:37:59 +0530

    err

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require("express")
 const app = express()
 const port = 3000
+const path = require("path")
+const ejsMate = require("ejs-mate")
 
-app.get("/", (req,res)=>{
-    res.send("home page")
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
+app.use(express.urlencoded({ extended: true }))
+app.engine("ejs", ejsMate)
+
+let activities = []
+
+app.get("/", (req, res) => {
+    res.render("homepage.ejs")
 })
 
-app.listen(port, ()=>{
+app.post("/", (req, res) => {
+    let newActivity = req.body.activity
+    activities.push(newActivity)
+    res.render("homepaje.ejs", { activities })
+})
+
+app.listen(port, () => {
     console.log("yeah listening")
 })
