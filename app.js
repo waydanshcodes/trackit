6885e1698d56cdc9b143c59aa638c59b9634e3d8
Vedansh Gupta <waydansh@gmail.com>
2024-07-17 19:41:29 +0530

    list

--- app.js
+++ app.js
@@ -3,24 +3,25 @@ const app = express()
 const port = 3000
 const path = require("path")
 const ejsMate = require("ejs-mate")
+const mongoose = require("mongoose")
 
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 app.use(express.urlencoded({ extended: true }))
 app.engine("ejs", ejsMate)
 
-let activities = []
+let tasks = []
 
 app.get("/", (req, res) => {
     res.render("homepage.ejs")
 })
 
 app.post("/", (req, res) => {
-    let newActivity = req.body.activity
-    activities.push(newActivity)
-    res.render("homepaje.ejs", { activities })
+    let newTask = req.body.task
+    tasks.push(newTask)
+    newTask = null
+    res.render("tasks.ejs", { tasks })
 })
-
 app.listen(port, () => {
     console.log("yeah listening")
 })
